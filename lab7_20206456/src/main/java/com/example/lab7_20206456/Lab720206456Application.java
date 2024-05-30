2d69d2b55b84e316e675abadf4b6e55ace9758d2
milenelarosa <a20206456@pucp.edu.pe>
2024-05-29 19:22:12 -0500

    First commit

---
+++ lab7_20206456/src/main/java/com/example/lab7_20206456/Lab720206456Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20206456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720206456Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720206456Application.class, args);
+    }
+
+}
